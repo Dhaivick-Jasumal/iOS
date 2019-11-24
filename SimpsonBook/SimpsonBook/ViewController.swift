acc7dfa2f9ef2ada5ce61b0c170823f7ec6d675e
Dhaivick <dhaivick18@gmail.com>
2019-11-24 20:31:29 +0530

    removed git repo inside projects

--- SimpsonBook/SimpsonBook/ViewController.swift
+++ SimpsonBook/SimpsonBook/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  SimpsonBook
 //
-//  Created by Atil Samancioglu on 11.07.2019.
-//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//  Created by Dhaivick on 11.07.2019.
+//  Copyright © 2019 Dhaivick. All rights reserved.
 //
 
 import UIKit
